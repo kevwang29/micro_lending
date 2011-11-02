02c89b893c1e7dcd6dcaaabddf9b25863bb6d9d8
Kevin Wang <kevinwang29@gmail.com>
2011-11-02 16:39:20 -0400

    fixed transaction by uid fixed BID naming error

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -36,7 +36,7 @@ class TransactionsController < ApplicationController
     @transaction = Transaction.find(:all, :conditions => [ "luid = ?" , params[:uid]])
     @correctMapping = Array.new
     @transaction.each{|transacs| 
-      x = Borrower.find(:first, :conditions =>["buid = ?", transacs[:BID]]);
+      x = Borrower.find(:first, :conditions =>["buid = ?", transacs[:buid]]);
       x[:amount] = transacs[:amount];
       @correctMapping.push(x);
       };
