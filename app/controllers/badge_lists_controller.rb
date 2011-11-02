3dbb25ba071b453037f43b8579520aff278056b2
Kevin Wang <kevinwang29@gmail.com>
2011-11-01 22:39:39 -0400

    fix badge_lists_controller tso that badge id is bid.

--- app/controllers/badge_lists_controller.rb
+++ app/controllers/badge_lists_controller.rb
@@ -35,7 +35,7 @@ class BadgeListsController < ApplicationController
   def byUid
     @badge_list = BadgeList.find(:all, :conditions => [ "luid = ?" , params[:uid]])
     @badges = Array.new
-    @badge_list.each{|badge| @badges.push(Badge.find(:first, :conditions =>["buid = ?", badge[:BUID]]))}
+    @badge_list.each{|badge| @badges.push(Badge.find(:first, :conditions =>["bid = ?", badge[:BUID]]))}
   
     respond_to do |format|
       format.html # new.html.erb
