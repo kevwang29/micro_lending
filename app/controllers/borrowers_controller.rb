2f4671390903b0d4ff4daf3018dc681bd7399e0c
Kevin Wang <kevinwang29@gmail.com>
2011-11-16 03:11:17 -0500

    Modified borrowers controller so that current_amount is sent

--- app/controllers/borrowers_controller.rb
+++ app/controllers/borrowers_controller.rb
@@ -2,11 +2,17 @@ class BorrowersController < ApplicationController
   # GET /borrowers
   # GET /borrowers.json
   def index
-    @borrowers = Borrower.all
-
+    @borrower = Borrower.all
+    @borrower.each{|b|
+      b[:current_amount] = 0;
+      @tran = Transaction.find(:all, :conditions => ["buid = ?" , b[:buid]]);
+        @tran.each{|tr|
+          b[:current_amount]=b[:current_amount]+tr[:amount]
+        }
+      }
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @borrowers }
+      format.json { render json: @borrower }
     end
   end
 
@@ -14,6 +20,14 @@ class BorrowersController < ApplicationController
   # GET /borrowers/1.json
   def show
     @borrower = Borrower.find(params[:id])
+
+    @borrower.each{|b|
+      b[:current_amount] = 0;
+      @tran = Transaction.find(:all, :conditions => ["buid = ?" , b[:buid]]);
+        @tran.each{|tr|
+          b[:current_amount]=b[:current_amount]+tr[:amount]
+        }
+      }
 
     respond_to do |format|
       format.html # show.html.erb
@@ -25,7 +39,13 @@ class BorrowersController < ApplicationController
   # GET /borrowers/new.json
   def new
     @borrower = Borrower.new
-
+    @borrower.each{|b|
+      b[:current_amount] = 0;
+      @tran = Transaction.find(:all, :conditions => ["buid = ?" , b[:buid]]);
+        @tran.each{|tr|
+          b[:current_amount]=b[:current_amount]+tr[:amount]
+        }
+      }
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @borrower }
@@ -35,13 +55,19 @@ class BorrowersController < ApplicationController
   # GET /borrowers/1/edit
   def edit
     @borrower = Borrower.find(params[:id])
+    @borrower.each{|b|
+      b[:current_amount] = 0;
+      @tran = Transaction.find(:all, :conditions => ["buid = ?" , b[:buid]]);
+        @tran.each{|tr|
+          b[:current_amount]=b[:current_amount]+tr[:amount]
+        }
+      }
   end
 
   # POST /borrowers
   # POST /borrowers.json
   def create
     @borrower = Borrower.new(params[:borrower])
-
     respond_to do |format|
       if @borrower.save
         format.html { redirect_to @borrower, notice: 'Borrower was successfully created.' }
@@ -83,11 +109,19 @@ class BorrowersController < ApplicationController
   
   def byCategory
     @borrower = Borrower.find(:all, :conditions => [ "category = ?" , params[:category]])
-
+    @borrower.each{|b|
+      b[:current_amount] = 0;
+      @tran = Transaction.find(:all, :conditions => ["buid = ?" , b[:buid]]);
+        @tran.each{|tr|
+          b[:current_amount]=b[:current_amount]+tr[:amount]
+        }
+      }
+      
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @borrower }
     end
   end
+  
 
 end
