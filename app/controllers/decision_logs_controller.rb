bca748d79d5e834cb76027e7367c0bce310ab1c5
Kevin Wang <kevinwang29@gmail.com>
2011-12-11 16:58:28 -0500

    changed getAllDecisions to return nil when decision is not present.

--- app/controllers/decision_logs_controller.rb
+++ app/controllers/decision_logs_controller.rb
@@ -97,7 +97,7 @@ class DecisionLogsController < ApplicationController
       @entry=Hash.new;
       @entry[:story] = StoryObject.find(decision[:story_id]);
       if decision[:decision_id]==-1 
-        @entry[:decision] = "";
+        @entry[:decision] = nil;
       else
        @entry[:decision]=DecisionObject.find(decision[:decision_id]);
       end
